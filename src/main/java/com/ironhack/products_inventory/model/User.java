4f2be92b81ad51b775fc43cbd134e08e4da47b53
Marc Pedron Pladevall <marcpedron@192.168.1.3>
2025-06-03 20:26:53 +0200

    add extra steps on DTO

--- src/main/java/com/ironhack/products_inventory/model/User.java
+++ src/main/java/com/ironhack/products_inventory/model/User.java
@@ -2,5 +2,5 @@ package com.ironhack.products_inventory.model;
 
 public class User {
 
-    //TODO with ID and enum: supplier or customer, list of orders
+    //TODO with ID and enum: supplier or customer, list of orders. FER AL FINAL
 }
